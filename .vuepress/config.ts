ed35be4618e326d16eecd7be22ecf17d4dba9f04
zengjielin <248717607@qq.com>
2024-10-22 21:46:24 +0800

    feat: 更新项目介绍及截图

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -25,6 +25,7 @@ export default defineUserConfig({
           text: "项目截图",
           children: [
             "易创宝管理系统",
+            "易创宝商品中心",
             "易创宝学习平台",
             "双十一大屏数据看板",
             "小黑探会展约车小程序",
@@ -104,8 +105,8 @@ export default defineUserConfig({
       type: "valine",
       // options 与 1.x 的 valineConfig 配置一致
       options: {
-        appId: "7Rd5srHP9piH7CCJ62sKuAW2-gzGzoHsz",
-        appKey: "9CZeENYCYA02EouZB0WWy3D1",
+        appId: "zlpggJ3MnvFMyid6H0YifAxj-gzGzoHsz",
+        appKey: "LxUJ2xD0zGUfzqXLTY7JMAkx",
         placeholder: "填写邮箱可以收到回复提醒哦！",
         // verify: true, // 验证码服务
         // notify: true,
